2a443e499f0d1929be35fcf17ae6e34a40650beb
Timothy Sanvictores <timtam8083@gmail.com>
2022-08-10 13:31:22 +1200

    fixed naming convention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='kiwrious_lib',
-	packages=find_packages(include=['Kiwriouslib']),
+	packages=find_packages(include=['kiwrious_lib']),
 	description='Python library for reading Kiwrious sensors',
 	install_requires=[],
 	setup_requires=['pytest-runner'],
